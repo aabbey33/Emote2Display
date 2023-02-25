904eef751a459af289e3e413b4acf5ad273799c8
Austin Abbey <aabbey33@gmail.com>
2023-02-25 15:07:19 -0800

    Initial Upload

---
+++ stt2e.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+
+import operator
+
+import speech_recognition as sr
+import text2emotion as te
+
+text = "" #Empty Text variable to be leveraged later by speech-to-text
+
+r = sr.Recognizer()
+
+##Emotion Bit
+
+def recognize(phrase):
+    emoteRec = te.get_emotion(phrase) #getting the emotion from a phrase
+    #print(emoteRec)
+    maxStat = max(emoteRec.items(), key=operator.itemgetter(1))[0]
+    print(maxStat)
+
+
+def recognize_speech_from_mic(recognizer, microphone):
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("recognizer must be recognizer instance")
+
+    if not isinstance(microphone,sr.Microphone):
+        raise TypeError("microphone bust be Microphone instance")
+
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+    
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+
+if __name__ == "__main__":
+
+
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+
+    while True:
+        text = recognize_speech_from_mic(recognizer, microphone)
+        if text["transcription"]:
+            recognize(text["transcription"])
+        if not text["success"]:
+            recognize(text["success"])
+        if text["error"]:
+            print("ERROR: {}".format(text["error"]))
+            
+        
+        
+
+
+
+#function majoris
+#while True:
+#    with sr.Microphone() as source2:
+#
+#        r.adjust_for_ambient_noise(source2, duration=0.2)
+#
+#        audio = r.listen(source2)
+#
+#        myText = r.recognize_google(audio) # I think I might move to Vosk API to have lower latency responses. latency will be the name of the game, after all
+#
+#        myText = myText.lower()
+#
+#        print("input: ",myText)
+#        #SpeakText(myText)
+#        recognize(myText)
+
+
