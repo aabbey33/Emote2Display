ccce64353cedd63487385bc370599ed69b911245
Austin Abbey <aabbey33@gmail.com>
2023-03-01 19:01:21 -0800

    Update stt2e.py
    Final functional version

--- stt2e.py
+++ stt2e.py
@@ -9,69 +9,8 @@ from threading import Thread
 
 import speech_recognition as sr
 import text2emotion as te
+import tkinter as tk
 
-
-# def recognize_speech_from_mic(recognizer, microphone):
-#     if not isinstance(recognizer, sr.Recognizer):
-#         raise TypeError("recognizer must be recognizer instance")
-#
-#     if not isinstance(microphone, sr.Microphone):
-#         raise TypeError("microphone bust be Microphone instance")
-#
-#     with sr.Microphone() as source:
-#         recognizer.adjust_for_ambient_noise(source)
-#         audio = recognizer.listen(source)
-#
-#     response = {
-#         "success": True,
-#         "error": None,
-#         "transcription": None
-#     }
-#
-#     try:
-#         response["transcription"] = recognizer.recognize_google(audio)
-#     except sr.RequestError:
-#         response["success"] = False
-#         response["error"] = "API unavailable"
-#     except sr.UnknownValueError:
-#         response["error"] = "Unable to recognize speech"
-#
-#     return response
-
-
-# if __name__ == "__main__":
-#
-#
-#     recognizer = sr.Recognizer()
-#     microphone = sr.Microphone()
-#
-#     while True:
-#         text = recognize_speech_from_mic(recognizer, microphone)
-#         if text["transcription"]:
-#             print(text["transcription"])
-#             Thread(target=recognize, args=(text["transcription"],lock))
-#         if not text["success"]:
-#             print(text["success"])
-#             Thread(target=recognize, args=(text["transcription"],lock))
-#         if text["error"]:
-#             print(f"ERROR: {text['error']}")
-
-
-# while True:
-#    with sr.Microphone() as source2:
-#
-#        r.adjust_for_ambient_noise(source2, duration=0.2)
-#
-#        audio = r.listen(source2)
-#
-# myText = r.recognize_google(audio) # I think I might move to Vosk API to have lower latency responses. latency will
-# be the name of the game, after all
-#
-#        myText = myText.lower()
-#
-#        print("input: ",myText)
-#        #SpeakText(myText)
-#        recognize(myText)
 
 class Listener:
 
@@ -83,7 +22,13 @@ class Listener:
         # Handle Visuals here and output
         print("placeholder")
 
+        self.root = tk.Tk()
+        self.label = tk.Label(text="Neutral", font=("Arial", 120, "bold"))
+        self.label.pack()
+
         Thread(target=self.run_listener).start()
+
+        self.root.mainloop()
 
         # do the loop
 
@@ -111,13 +56,7 @@ class Listener:
 
         max_stat = max(emote_rec.items(), key=operator.itemgetter(1))[0]
         print(max_stat)
+        self.label.config(text=f"{max_stat}")
 
 
 Listener()
-
-# def recognize(phrase):
-#     emoteRec = te.get_emotion(phrase)  # getting the emotion from a phrase
-#     # print(emoteRec)
-#     maxStat = max(emoteRec.items(), key=operator.itemgetter(1))[0]
-#     print(maxStat)
-#     lock.release()
