049db2957fa239e8b1f0a8eb8fef1f5339a7a524
Austin Abbey <aabbey33@gmail.com>
2023-03-04 16:58:18 -0800

    Create LiveEmotionML.py

---
+++ LiveEmotionML.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# Written by Austin Abbey
+# With assistance from
+# https://www.thepythoncode.com/article/building-a-speech-emotion-recognizer-using-sklearn
+#
+# Initially inspired by
+# https://github.com/Zolmeister/AudioMan
+# https://zolmeister.com/2012/10/back-light-music-leds.html
+
+
